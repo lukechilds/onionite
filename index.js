c0f284a75d60a067c5b7dee5bea131b484f9ddd8
Luke Childs <lukechilds123@gmail.com>
2016-08-31 19:08:28 +0100

    Pass req/res through to nunjuck

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const viewModel = require('./viewModels');
 const app       = express();
 const port      = process.env.port || 3000;
 
-nunjucks.configure('views', { express: app });
+app.set('nunjuckEnv', nunjucks.configure('views', { express: app }));
+
+app.use((req, res, next) => {
+  req.app.settings.nunjuckEnv
+    .addGlobal('req', req)
+    .addGlobal('res', res);
+
+  next();
+});
 
 app.get('/', viewModel.listing);
 
