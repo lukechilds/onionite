081574acf9df939522c14ee7110aae4cd27f923b
Luke Childs <lukechilds123@gmail.com>
2016-09-01 13:04:21 +0100

    Indent map

--- viewModels/listing.js
+++ viewModels/listing.js
@@ -12,15 +12,16 @@ module.exports = (req, res) => {
       return Promise.all(nodes.map(node => onionoo.details({ lookup: node.f || node.h })));
     })
     .then(summaryDetails => {
-      const nodes = summaryDetails.map(details => {
-        if(details.relays[0]) {
-          details.relays[0].type = 'relay';
-          return details.relays[0];
-        } else if(details.bridges[0]) {
-          details.bridges[0].type = 'bridge';
-          return details.bridges[0];
-        }
-      });
+      const nodes = summaryDetails
+        .map(details => {
+          if(details.relays[0]) {
+            details.relays[0].type = 'relay';
+            return details.relays[0];
+          } else if(details.bridges[0]) {
+            details.bridges[0].type = 'bridge';
+            return details.bridges[0];
+          }
+        });
       return res.render('listing.html', { nodes: nodes });
     });
 }
