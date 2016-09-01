855edae7a30c47fb78d143899137a4f7375aa0f0
Luke Childs <lukechilds123@gmail.com>
2016-09-01 13:35:58 +0100

    Setup nunjuck filters and add filter for node bandwidth

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const nunjucks  = require('nunjucks');
-const express   = require('express');
-const viewModel = require('./viewModels');
-const app       = express();
-const port      = process.env.port || 3000;
+const nunjucks        = require('nunjucks');
+const express         = require('express');
+const nunjuckFilters  = require('./lib/nunjuck-filters');
+const viewModel       = require('./viewModels');
+const app             = express();
+const port            = process.env.port || 3000;
 
 app.set('nunjucksEnv', nunjucks.configure('views', { express: app }));
+nunjuckFilters(app);
 
 app.use((req, res, next) => {
   req.app.settings.nunjucksEnv
