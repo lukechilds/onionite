43eeb9fe457a85b1912abefb10b7c6c8d4713044
Luke Childs <lukechilds123@gmail.com>
2016-09-03 08:44:12 +0100

    Add node template

--- controllers/node.js
+++ controllers/node.js
@@ -1,5 +1,11 @@
 const tor = require('../lib/tor');
 
 module.exports = (req, res, next) => {
-  tor.node(req.params.id).then(node => res.json(node))
+  tor.node(req.params.id)
+    .then(node => res.render('node.html', {
+      node: node,
+    }))
+    .catch(error => res.render('node.html', {
+      error: error
+    }));
 }
