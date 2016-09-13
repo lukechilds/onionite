eaf5fd2fe5b5c1c8a64a5403ed6b63e9e4399884
Luke Childs <lukechilds123@gmail.com>
2016-09-13 16:51:03 +0100

    Write DOM load check more efficiently

--- public/assets/enhancements.js
+++ public/assets/enhancements.js
@@ -1,9 +1,7 @@
 // Check if DOM has already loaded as we're loading async
-if(['interactive', 'complete'].indexOf(document.readyState) >= 0) {
-  init();
-} else {
-  document.addEventListener('DOMContentLoaded', init);
-}
+['interactive', 'complete'].indexOf(document.readyState) >= 0
+  ? init()
+  : document.addEventListener('DOMContentLoaded', init);
 
 // When DOM is ready
 function init() {
