7232271d72c406a03283d8e18d6a8e969e4a0283
Luke Childs <lukechilds123@gmail.com>
2016-09-16 13:35:56 +0100

    Move ios check into function

--- public/assets/enhancements.js
+++ public/assets/enhancements.js
@@ -1,5 +1,16 @@
 (function() {
 
+  // Add ios class to body on iOS devices
+  function iosBodyClass() {
+    if(
+      /iPad|iPhone|iPod/.test(navigator.userAgent)
+      && !window.MSStream
+      && document.body.classList
+    ) {
+      document.body.classList.add('ios');
+    }
+  }
+
   // Check if DOM has already loaded as we're loading async
   ['interactive', 'complete'].indexOf(document.readyState) >= 0
     ? init()
@@ -9,13 +20,7 @@
   function init() {
 
     // Check for iOS
-    if(
-      /iPad|iPhone|iPod/.test(navigator.userAgent)
-      && !window.MSStream
-      && document.body.classList
-    ) {
-      document.body.classList.add('ios');
-    }
+    iosBodyClass();
   }
 
 })();
